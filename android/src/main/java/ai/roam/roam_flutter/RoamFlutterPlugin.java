257e71f7987af924eaeca7e083eb912d2be3348c
jothipriyadharshanr <joe@geospark.co>
2021-02-22 13:54:13 +0530

    Added Java Support

---
+++ android/src/main/java/ai/roam/roam_flutter/RoamFlutterPlugin.java
@@ -0,0 +1,53 @@
+package ai.roam.roam_flutter;
+
+import androidx.annotation.NonNull;
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.PluginRegistry.Registrar;
+
+/** RoamFlutterPlugin */
+public class RoamFlutterPlugin implements FlutterPlugin, MethodCallHandler {
+  /// The MethodChannel that will the communication between Flutter and native Android
+  ///
+  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+  /// when the Flutter Engine is detached from the Activity
+  private MethodChannel channel;
+
+  @Override
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "roam_flutter");
+    channel.setMethodCallHandler(this);
+  }
+
+  @Override
+  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+    if (call.method.equals("getPlatformVersion")) {
+      result.success("Android " + android.os.Build.VERSION.RELEASE);
+    } else {
+      result.notImplemented();
+    }
+    // try {
+    //   switch (call.method) {
+    //     case METHOD_INITIALIZE:
+    //       final String publishableKey = call.argument("publishableKey");
+    //       break;
+
+    //     default:
+    //       result.notImplemented();
+    //       break;
+    //   }
+
+    // } catch (Error e) {
+    //   result.error(e.toString(), e.getMessage(), e.getCause());
+    // }
+  }
+
+  @Override
+  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+    channel.setMethodCallHandler(null);
+  }
+}
