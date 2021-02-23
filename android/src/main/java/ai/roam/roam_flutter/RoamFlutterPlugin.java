d7b0b53ab30a2378eef50233a88e7d71fd472c83
jothipriyadharshanr <joe@geospark.co>
2021-02-23 17:49:14 +0530

    Added Initialise Method

--- android/src/main/java/ai/roam/roam_flutter/RoamFlutterPlugin.java
+++ android/src/main/java/ai/roam/roam_flutter/RoamFlutterPlugin.java
@@ -1,53 +1,97 @@
 package ai.roam.roam_flutter;
 
-import androidx.annotation.NonNull;
+import android.app.Activity;
+import android.content.Context;
 
+import com.geospark.lib.GeoSpark;
+
+import androidx.annotation.NonNull;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
+import io.flutter.plugin.common.PluginRegistry;
 
 /** RoamFlutterPlugin */
-public class RoamFlutterPlugin implements FlutterPlugin, MethodCallHandler {
+public class RoamFlutterPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private MethodChannel channel;
+  private Activity activity;
+
+  private static final String METHOD_INITIALIZE = "initialize";
+
+  private Context context;
+
+  private void setContext(Context context) {
+    this.context = context;
+  }
+
+  @Override
+  public void onAttachedToActivity(ActivityPluginBinding activityPluginBinding) {
+    this.activity = activityPluginBinding.getActivity();
+  }
+
+  @Override
+  public void onDetachedFromActivityForConfigChanges() {
+    this.activity = null;
+  }
+
+  @Override
+  public void onReattachedToActivityForConfigChanges(ActivityPluginBinding activityPluginBinding) {
+    this.activity = activityPluginBinding.getActivity();
+  }
+
+  @Override
+  public void onDetachedFromActivity() {
+    this.activity = null;
+  }
 
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+    this.context = flutterPluginBinding.getApplicationContext();
     channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "roam_flutter");
     channel.setMethodCallHandler(this);
   }
 
+  public static void registerWith(PluginRegistry.Registrar registrar) {
+    final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_radar_io");
+    final RoamFlutterPlugin plugin = new RoamFlutterPlugin();
+    plugin.setContext(registrar.context());
+    channel.setMethodCallHandler(plugin);
+  }
+
   @Override
   public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
-    } else {
-      result.notImplemented();
-    }
-    // try {
-    //   switch (call.method) {
-    //     case METHOD_INITIALIZE:
-    //       final String publishableKey = call.argument("publishableKey");
-    //       break;
+     try {
+       switch (call.method) {
+         case "getPlatformVersion":
+           result.success("Android " + android.os.Build.VERSION.RELEASE);
+           break;
 
-    //     default:
-    //       result.notImplemented();
-    //       break;
-    //   }
+         case METHOD_INITIALIZE:
+           final String publishKey = call.argument("publishKey");
+           GeoSpark.initialize(this.context, publishKey);
+           break;
 
-    // } catch (Error e) {
-    //   result.error(e.toString(), e.getMessage(), e.getCause());
-    // }
+         default:
+           result.notImplemented();
+           break;
+       }
+
+     } catch (Error e) {
+       result.error(e.toString(), e.getMessage(), e.getCause());
+     }
   }
 
   @Override
   public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+    this.context = null;
     channel.setMethodCallHandler(null);
   }
 }
